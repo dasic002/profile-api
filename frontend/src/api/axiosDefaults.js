ac5c8fdaef3e0719b04605763d771246f1e308de
dasic002 <dasic002@yahoo.co.uk>
2025-03-11 14:56:10 +0000

    Prepare frontend for deployment

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://dasic002-drf-api-85265eacf671.herokuapp.com/";
+// axios.defaults.baseURL = "https://dasic002-drf-api-85265eacf671.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
